59dc7941117470931e03c43a36b3acedbf3d4f3e
muthubharathi <muthubharathi.r@gmail.com>
2016-09-16 21:13:15 +0530

    Added student controller
    Change-Id: Ic11db0c1397436fa66f5170468d242534ddba3ec
    Signed-off-by: muthubharathi <muthubharathi.r@gmail.com>

--- src/main/java/com/school/controller/StudentController.java
+++ src/main/java/com/school/controller/StudentController.java
@@ -1,5 +1,30 @@
 package com.school.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/student")
 public class StudentController {
 
+	@RequestMapping("/add")
+	public String addStudent(){
+		return "add_student";
+	}
+	
+	@RequestMapping("/modify")
+	public String modifyStudent(){
+		return "modify_student";
+	}
+	
+	@RequestMapping("/delete")
+	public String deleteStudent(){
+		return "delete_student";
+	}
+	
+	@RequestMapping("/export")
+	public String exportStudent(){
+		return "export_student";
+	}
+	
 }
